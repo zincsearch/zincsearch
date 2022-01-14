d8be2df9f308e4afea68892355f5c1a87d9d45fc
safeie <hengfei.yang@gmail.com>
2022-01-14 17:04:18 +0800

    optimize create index

--- pkg/handlers/CreateIndex.go
+++ pkg/handlers/CreateIndex.go
@@ -11,6 +11,11 @@ func CreateIndex(c *gin.Context) {
 	var newIndex core.Index
 	c.BindJSON(&newIndex)
 
+	if newIndex.Name == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "index.name should be not empty"})
+		return
+	}
+
 	if ok, storage := core.IndexExists(newIndex.Name); ok {
 		c.JSON(http.StatusOK, gin.H{
 			"result":       "Index: " + newIndex.Name + " exists",
