d1b515a60f015cae160e885a76fdba5e39c0925b
safeie <hengfei.yang@gmail.com>
2022-01-14 17:04:30 +0800

    add tests

--- test/api_test.go
+++ test/api_test.go
@@ -8,8 +8,14 @@ import (
 	"testing"
 
 	"github.com/prabhatsharma/zinc/pkg/auth"
+	meta "github.com/prabhatsharma/zinc/pkg/meta/v1"
 	. "github.com/smartystreets/goconvey/convey"
 )
+
+type userLoginResponse struct {
+	User      auth.ZincUser `json:"user"`
+	Validated bool          `json:"validated"`
+}
 
 func TestApiStandard(t *testing.T) {
 	Convey("test zinc api", t, func() {
@@ -20,44 +26,115 @@ func TestApiStandard(t *testing.T) {
 				resp := request("POST", "/api/login", body)
 				So(resp.Code, ShouldEqual, http.StatusOK)
 
-				var data struct {
-					User auth.ZincUser `json:"user"`
-				}
+				data := new(userLoginResponse)
 				err := json.Unmarshal(resp.Body.Bytes(), &data)
 				So(err, ShouldBeNil)
-				So(data.User.Role, ShouldEqual, "admin")
+				So(data.Validated, ShouldBeTrue)
 			})
 			Convey("with error username or password", func() {
+				body := bytes.NewBuffer(nil)
+				body.WriteString(fmt.Sprintf(`{"_id": "%s", "password": "xxx"}`, username))
+				resp := request("POST", "/api/login", body)
+				So(resp.Code, ShouldEqual, http.StatusOK)
+
+				data := new(userLoginResponse)
+				err := json.Unmarshal(resp.Body.Bytes(), &data)
+				So(err, ShouldBeNil)
+				So(data.Validated, ShouldBeFalse)
 			})
 		})
 
 		Convey("PUT /api/user", func() {
+			username := "user1"
+			password := "123456"
 			Convey("create user with payload", func() {
+				// create user
+				body := bytes.NewBuffer(nil)
+				body.WriteString(fmt.Sprintf(`{"_id":"%s","name":"%s","password":"%s","role":"admin"}`, username, username, password))
+				resp := request("PUT", "/api/user", body)
+				So(resp.Code, ShouldEqual, http.StatusOK)
+
+				// login check
+				body.Reset()
+				body.WriteString(fmt.Sprintf(`{"_id":"%s","password":"%s"}`, username, password))
+				resp = request("POST", "/api/login", body)
+				So(resp.Code, ShouldEqual, http.StatusOK)
+
+				data := new(userLoginResponse)
+				err := json.Unmarshal(resp.Body.Bytes(), &data)
+				So(err, ShouldBeNil)
+				So(data.Validated, ShouldBeTrue)
+			})
+			Convey("update user", func() {
+				// update user
+				body := bytes.NewBuffer(nil)
+				body.WriteString(fmt.Sprintf(`{"_id":"%s","name":"%s-updated","password":"%s","role":"admin"}`, username, username, password))
+				resp := request("PUT", "/api/user", body)
+				So(resp.Code, ShouldEqual, http.StatusOK)
+
+				// check updated
+				_, userNew, _ := auth.GetUser(username)
+				So(userNew.Name, ShouldEqual, fmt.Sprintf("%s-updated", username))
 			})
 			Convey("create user with error input", func() {
+				body := bytes.NewBuffer(nil)
+				body.WriteString(`xxx`)
+				resp := request("PUT", "/api/user", body)
+				So(resp.Code, ShouldEqual, http.StatusBadRequest)
 			})
 		})
 
 		Convey("DELETE /api/user/:userID", func() {
 			Convey("delete user with exist userid", func() {
+				username := "user1"
+				resp := request("DELETE", "/api/user/"+username, nil)
+				So(resp.Code, ShouldEqual, http.StatusOK)
+
+				// check user exist
+				exist, _, _ := auth.GetUser(username)
+				So(exist, ShouldBeFalse)
 			})
 			Convey("delete user with not exist userid", func() {
-			})
-			Convey("delete user with error input", func() {
+				resp := request("DELETE", "/api/user/userNotExist", nil)
+				So(resp.Code, ShouldEqual, http.StatusOK)
 			})
 		})
 
 		Convey("GET /api/users", func() {
+			resp := request("GET", "/api/users", nil)
+			So(resp.Code, ShouldEqual, http.StatusOK)
+
+			data := new(meta.SearchResponse)
+			err := json.Unmarshal(resp.Body.Bytes(), data)
+			So(err, ShouldBeNil)
+			So(data.Hits.Total.Value, ShouldEqual, 1)
+			So(data.Hits.Hits[0].ID, ShouldEqual, "admin")
 		})
 
 		Convey("PUT /api/index", func() {
 			Convey("create index with payload", func() {
+				body := bytes.NewBuffer(nil)
+				body.WriteString(fmt.Sprintf(`{"name":"%s","storage_type":"disk"}`, "newindex"))
+				resp := request("PUT", "/api/index", body)
+				So(resp.Code, ShouldEqual, http.StatusOK)
+				So(resp.Body.String(), ShouldEqual, `{"result":"Index: newindex created","storage_type":"disk"}`)
 			})
 			Convey("create index with error input", func() {
+				body := bytes.NewBuffer(nil)
+				body.WriteString(fmt.Sprintf(`{"name":"%s","storage_type":"disk"}`, ""))
+				resp := request("PUT", "/api/index", body)
+				So(resp.Code, ShouldEqual, http.StatusBadRequest)
 			})
 		})
 
 		Convey("GET /api/index", func() {
+			resp := request("GET", "/api/index", nil)
+			So(resp.Code, ShouldEqual, http.StatusOK)
+
+			data := make(map[string]interface{})
+			err := json.Unmarshal(resp.Body.Bytes(), &data)
+			So(err, ShouldBeNil)
+			So(len(data), ShouldBeGreaterThanOrEqualTo, 1)
 		})
 
 		Convey("DELETE /api/index/:indexName", func() {
