8f6451db9792229e1b8a395113c2ec41c86c0342
hengfeiyang <hengfei.yang@gmail.com>
2022-02-24 02:04:15 +0800

    add analyzer

--- pkg/uquery/v2/analyzer/analyzer.go
+++ pkg/uquery/v2/analyzer/analyzer.go
@@ -2,12 +2,9 @@ package analyzer
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/blugelabs/bluge/analysis"
 	"github.com/blugelabs/bluge/analysis/analyzer"
-	"github.com/blugelabs/bluge/analysis/char"
-	"github.com/blugelabs/bluge/analysis/token"
 
 	"github.com/prabhatsharma/zinc/pkg/errors"
 	meta "github.com/prabhatsharma/zinc/pkg/meta/v2"
@@ -32,24 +29,34 @@ func Request(data *meta.IndexAnalysis) (map[string]*analysis.Analyzer, error) {
 		return nil, err
 	}
 
+	tokenizers, err := RequestTokenizer(data.Tokenizer)
+	if err != nil {
+		return nil, err
+	}
+
 	analyzers := make(map[string]*analysis.Analyzer)
 	for name, v := range data.Analyzer {
-		var ana *analysis.Analyzer
 		if v.Tokenizer == "" {
 			return nil, errors.New(errors.ErrorTypeParsingException, fmt.Sprintf("[analyzer] [%s] is missing tokenizer", name))
 		}
 
+		var ok bool
+		zer, err := RequestTokenizerSingle(v.Tokenizer, nil)
+		if zer != nil && err == nil {
+			// use standard tokenizer
+		} else {
+			if zer, ok = tokenizers[v.Tokenizer]; !ok {
+				return nil, errors.New(errors.ErrorTypeParsingException, fmt.Sprintf("[analyzer] [%s] used undifined tokenizer %s", name, v.Tokenizer))
+			}
+		}
+
 		chars := make([]analysis.CharFilter, 0, len(v.CharFilter))
-		for _, filter := range v.CharFilter {
-			switch filter {
-			case "ascii_folding":
-				chars = append(chars, char.NewASCIIFoldingFilter())
-			case "html", "html_strip":
-				chars = append(chars, char.NewHTMLCharFilter())
-			case "zero_width_non_joiner":
-				chars = append(chars, char.NewZeroWidthNonJoinerCharFilter())
-			default:
-				if v, ok := charFilters[filter]; ok {
+		for _, name := range v.CharFilter {
+			filter, err := RequestCharFilterSingle(name, nil)
+			if filter != nil && err == nil {
+				chars = append(chars, filter)
+			} else {
+				if v, ok := charFilters[name]; ok {
 					chars = append(chars, v)
 				} else {
 					return nil, errors.New(errors.ErrorTypeParsingException, fmt.Sprintf("[analyzer] [%s] used undefined char_filter [%s]", name, filter))
@@ -58,22 +65,12 @@ func Request(data *meta.IndexAnalysis) (map[string]*analysis.Analyzer, error) {
 		}
 
 		tokens := make([]analysis.TokenFilter, 0, len(v.TokenFilter))
-		for _, filter := range v.TokenFilter {
-			switch filter {
-			case "apostrophe":
-				tokens = append(tokens, token.NewApostropheFilter())
-			case "camel_case":
-				tokens = append(tokens, token.NewCamelCaseFilter())
-			case "lower_case":
-				tokens = append(tokens, token.NewLowerCaseFilter())
-			case "porter":
-				tokens = append(tokens, token.NewPorterStemmer())
-			case "reverse":
-				tokens = append(tokens, token.NewReverseFilter())
-			case "unique":
-				tokens = append(tokens, token.NewUniqueTermFilter())
-			default:
-				if v, ok := tokenFilters[filter]; ok {
+		for _, name := range v.TokenFilter {
+			filter, err := RequestTokenFilterSingle(name, nil)
+			if filter != nil && err == nil {
+				tokens = append(tokens, filter)
+			} else {
+				if v, ok := tokenFilters[name]; ok {
 					tokens = append(tokens, v)
 				} else {
 					return nil, errors.New(errors.ErrorTypeParsingException, fmt.Sprintf("[analyzer] [%s] used undefined token_filter [%s]", name, filter))
@@ -81,20 +78,7 @@ func Request(data *meta.IndexAnalysis) (map[string]*analysis.Analyzer, error) {
 			}
 		}
 
-		v.Tokenizer = strings.ToLower(v.Tokenizer)
-		switch v.Tokenizer {
-		case "standard":
-			ana = analyzer.NewStandardAnalyzer()
-		case "keyword":
-			ana = analyzer.NewKeywordAnalyzer()
-		case "simple":
-			ana = analyzer.NewSimpleAnalyzer()
-		case "web":
-			ana = analyzer.NewWebAnalyzer()
-		default:
-			return nil, errors.New(errors.ErrorTypeXContentParseException, fmt.Sprintf("[analyzer] [%s] doesn't support tokenizer [%s]", name, v.Tokenizer))
-		}
-
+		ana := &analysis.Analyzer{Tokenizer: zer}
 		if len(chars) > 0 {
 			ana.CharFilters = append(ana.CharFilters, chars...)
 		}
