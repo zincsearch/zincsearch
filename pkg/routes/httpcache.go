5411d9e9da3fe371bfea70ddced1bbeb9e09f239
hengfeiyang <hengfei.yang@gmail.com>
2022-03-21 16:23:23 +0800

    fix cache for ui assets

--- pkg/routes/httpcache.go
+++ pkg/routes/httpcache.go
@@ -10,7 +10,7 @@ import (
 func HTTPCacheForUI(r *gin.Engine) {
 	r.Use(func(c *gin.Context) {
 		if c.Request.Method == "GET" || c.Request.Method == "HEAD" {
-			if strings.Contains(c.Request.RequestURI, "/ui/") {
+			if strings.Contains(c.Request.RequestURI, "/ui/assets/") {
 				c.Writer.Header().Set("cache-control", "public, max-age=2592000")
 				c.Writer.Header().Set("expires", time.Now().Add(time.Hour*24*30).Format(time.RFC1123))
 			}
