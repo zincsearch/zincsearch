7777204dd10e7c91feca9ac10165b0777886963a
yanghengfei <hengfei.yang@gmail.com>
2022-04-21 23:43:59 +0800

    feat: add shutdown

--- cmd/zinc/main.go
+++ cmd/zinc/main.go
@@ -1,8 +1,17 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+
+	"github.com/zinclabs/zinc/pkg/core"
 	"github.com/zinclabs/zinc/pkg/routes"
 	"github.com/zinclabs/zinc/pkg/zutils"
 )
@@ -17,5 +26,44 @@ func main() {
 
 	// Run the server
 	PORT := zutils.GetEnv("PORT", "4080")
-	r.Run(":" + PORT)
+	server := &http.Server{
+		Addr:    ":" + PORT,
+		Handler: r,
+	}
+
+	shutdown(func(grace bool) {
+		core.CloseIndexes() // close all indexes
+		log.Info().Msgf("Index closed")
+		if grace {
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*60)
+			defer cancel()
+			if err := server.Shutdown(ctx); err != nil {
+				log.Fatal().Msgf("Server Shutdown:", err)
+			}
+		} else {
+			server.Close()
+		}
+	})
+
+	if err := server.ListenAndServe(); err != nil {
+		if err == http.ErrServerClosed {
+			log.Info().Msgf("Server closed under request")
+		} else {
+			log.Fatal().Msgf("Server closed unexpect")
+		}
+	}
+
+	log.Info().Msgf("Server shutdown ok")
+}
+
+//shutdown support twice signal must exit
+func shutdown(stop func(grace bool)) {
+	sig := make(chan os.Signal, 2)
+	signal.Notify(sig, syscall.SIGQUIT, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		s := <-sig
+		go stop(s != syscall.SIGQUIT)
+		<-sig
+		os.Exit(128 + int(s.(syscall.Signal))) // second signal. Exit directly.
+	}()
 }
