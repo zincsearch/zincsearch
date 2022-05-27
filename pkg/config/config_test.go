32b72d06365321d804022d9b7f9247cd7e099f19
hengfeiyang <hengfei.yang@gmail.com>
2022-05-28 01:39:54 +0800

    update unit tests for config

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -16,31 +16,45 @@
 package config
 
 import (
+	"os"
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestConfig(t *testing.T) {
-	assert.Equal(t, "", Global.GinMode)
-	assert.Equal(t, "4080", Global.ServerPort)
-	// assert.Equal(t, "node", Global.ServerMode)
-	assert.Equal(t, "1", Global.NodeID)
-	assert.Equal(t, "./data", Global.DataPath)
-	assert.Equal(t, true, Global.SentryEnable)
-	assert.Equal(t, true, Global.TelemetryEnable)
-	assert.Equal(t, false, Global.PrometheusEnable)
+	t.Run("prepare", func(t *testing.T) {
+		os.Setenv("ZINC_SERVER_MODE", "node")
+		os.Setenv("ZINC_NODE_ID", "x")
+		os.Setenv("ZINC_ETCD_ENDPOINTS", "localhost:2379")
+	})
 
-	assert.Equal(t, 1024, Global.BatchSize)
-	assert.Equal(t, 10000, Global.MaxResults)
-	assert.Equal(t, 1000, Global.AggregationTermsSize)
+	t.Run("check", func(t *testing.T) {
+		c := new(config)
+		rv := reflect.ValueOf(c).Elem()
+		loadConfig(rv)
 
-	// assert.Equal(t, []string(nil), Global.Etcd.Endpoints)
+		assert.Equal(t, "", c.GinMode)
+		assert.Equal(t, "4080", c.ServerPort)
+		assert.Equal(t, "node", c.ServerMode)
+		assert.Equal(t, "x", c.NodeID)
+		assert.Equal(t, "./data", c.DataPath)
+		assert.Equal(t, true, c.SentryEnable)
+		assert.Equal(t, true, c.TelemetryEnable)
+		assert.Equal(t, false, c.PrometheusEnable)
 
-	assert.Equal(t, "", Global.S3.Bucket)
-	assert.Equal(t, "", Global.MinIO.Endpoint)
+		assert.Equal(t, 1024, c.BatchSize)
+		assert.Equal(t, 10000, c.MaxResults)
+		assert.Equal(t, 1000, c.AggregationTermsSize)
 
-	assert.Equal(t, false, Global.Plugin.GSE.Enable)
-	assert.Equal(t, "small", Global.Plugin.GSE.DictEmbed)
-	assert.Equal(t, "./plugins/gse/dict", Global.Plugin.GSE.DictPath)
+		assert.Equal(t, []string{"localhost:2379"}, c.Etcd.Endpoints)
+
+		assert.Equal(t, "", c.S3.Bucket)
+		assert.Equal(t, "", c.MinIO.Endpoint)
+
+		assert.Equal(t, false, c.Plugin.GSE.Enable)
+		assert.Equal(t, "small", c.Plugin.GSE.DictEmbed)
+		assert.Equal(t, "./plugins/gse/dict", c.Plugin.GSE.DictPath)
+	})
 }
