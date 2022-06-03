54e8bfcd1b7ff4fb5daa4c140e1fa369bcad2311
Prabhat Sharma <hi.prabhat@gmail.com>
2022-06-02 18:19:01 -0700

    i18n bundler configs

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,20 +1,27 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 
 // https://vitejs.dev/config/
 /**
  * @type {import('vite').UserConfig}
  */
 export default defineConfig({
+  define: {
+    __VUE_I18N_FULL_INSTALL__: true,
+    __VUE_I18N_LEGACY_API__: false,
+    __INTLIFY_PROD_DEVTOOLS__: false,
+  },
   server: {
     port: 8080,
   },
   base: "./",
-  plugins: [vue({
-    template: { transformAssetUrls }
-  }),
-  quasar({
-    sassVariables: 'src/styles/quasar-variables.sass'
-  })]
-})
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+    quasar({
+      sassVariables: "src/styles/quasar-variables.sass",
+    }),
+  ],
+});
