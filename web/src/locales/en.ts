0f0e6b685fe17a8ea260b5addd69ffca0b0cb1da
xiaojun207 <xiaojun207@126.com>
2022-07-01 01:25:55 +0800

    Add document link in UI

--- web/src/locales/en.ts
+++ web/src/locales/en.ts
@@ -20,6 +20,7 @@ export default {
       account: "Account",
       feedback: "Feedback",
       zincSearch: "Zinc Search",
+      documentation: "Documentation",
     },
     about: {
       name: "Zinc",
