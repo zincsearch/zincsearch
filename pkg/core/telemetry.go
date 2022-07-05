ad3b3084fc84608affba18b1a6e4668f117c1fe1
Prabhat Sharma <hi.prabhat@gmail.com>
2022-07-05 06:48:53 -0700

    upgraded gopsutil for openbsd

--- pkg/core/telemetry.go
+++ pkg/core/telemetry.go
@@ -23,8 +23,8 @@ import (
 	"github.com/robfig/cron/v3"
 	"github.com/rs/zerolog/log"
 	"github.com/segmentio/analytics-go/v3"
-	"github.com/shirou/gopsutil/cpu"
-	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/mem"
 
 	"github.com/zinclabs/zinc/pkg/config"
 	"github.com/zinclabs/zinc/pkg/errors"
