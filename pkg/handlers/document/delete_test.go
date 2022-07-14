c7b58f014786619e7c4acec5ffd2686288bd8600
hengfeiyang <hengfei.yang@gmail.com>
2022-07-14 21:35:41 +0800

    Test

--- pkg/handlers/document/delete_test.go
+++ pkg/handlers/document/delete_test.go
@@ -81,42 +81,34 @@ func TestDelete(t *testing.T) {
 		},
 	}
 
-	t.Run("prepare", func(t *testing.T) {
-		// create a document
-		data := map[string]interface{}{
-			"_id":  "1",
-			"name": "user",
-			"role": "create",
-		}
-		params := map[string]string{
-			"target": "TestDocumentDelete.index_1",
-		}
-		c, w := utils.NewGinContext()
-		utils.SetGinRequestData(c, data)
-		utils.SetGinRequestParams(c, params)
-		CreateUpdate(c)
-		assert.Equal(t, http.StatusOK, w.Code)
-		assert.Contains(t, w.Body.String(), `"id":"1"`)
+	// create a document
+	data := map[string]interface{}{
+		"_id":  "1",
+		"name": "user",
+		"role": "create",
+	}
+	params := map[string]string{
+		"target": "TestDocumentDelete.index_1",
+	}
+	c, w := utils.NewGinContext()
+	utils.SetGinRequestData(c, data)
+	utils.SetGinRequestParams(c, params)
+	CreateUpdate(c)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Contains(t, w.Body.String(), `"id":"1"`)
 
-		// wait for WAL write to index
-		time.Sleep(time.Second * 3)
-	})
+	// wait for WAL write to index
+	time.Sleep(time.Second)
 
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c, w := utils.NewGinContext()
-			utils.SetGinRequestParams(c, tt.args.params)
-			Delete(c)
-			assert.Equal(t, tt.args.code, w.Code)
-			assert.Contains(t, w.Body.String(), tt.args.result)
-		})
+		c, w := utils.NewGinContext()
+		utils.SetGinRequestParams(c, tt.args.params)
+		Delete(c)
+		assert.Equal(t, tt.args.code, w.Code)
+		assert.Contains(t, w.Body.String(), tt.args.result)
 	}
 
-	t.Run("cleanup", func(t *testing.T) {
-		// wait for WAL write to index
-		time.Sleep(time.Second * 3)
+	err := core.DeleteIndex("TestDocumentDelete.index_1")
+	assert.NoError(t, err)
 
-		err := core.DeleteIndex("TestDocumentDelete.index_1")
-		assert.NoError(t, err)
-	})
 }
