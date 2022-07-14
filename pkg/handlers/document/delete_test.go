cf39d521dc636353f042d52f65bfe07d80cbaf4c
hengfeiyang <hengfei.yang@gmail.com>
2022-07-14 19:43:58 +0800

    Fix unit tests

--- pkg/handlers/document/delete_test.go
+++ pkg/handlers/document/delete_test.go
@@ -99,7 +99,7 @@ func TestDelete(t *testing.T) {
 		assert.Contains(t, w.Body.String(), `"id":"1"`)
 
 		// wait for WAL write to index
-		time.Sleep(time.Second)
+		time.Sleep(time.Second * 2)
 	})
 
 	for _, tt := range tests {
