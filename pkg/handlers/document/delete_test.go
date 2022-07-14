46b38bba321172b4e4d42225c32df8b89d6a5dc2
hengfeiyang <hengfei.yang@gmail.com>
2022-07-14 22:31:24 +0800

    Test

--- pkg/handlers/document/delete_test.go
+++ pkg/handlers/document/delete_test.go
@@ -98,7 +98,7 @@ func TestDelete(t *testing.T) {
 	assert.Contains(t, w.Body.String(), `"id":"1"`)
 
 	// wait for WAL write to index
-	time.Sleep(time.Second * 3)
+	time.Sleep(time.Second * 2)
 
 	for _, tt := range tests {
 		c, w := utils.NewGinContext()
