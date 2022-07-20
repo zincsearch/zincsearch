6e0fab19205c21ce9f9a93647a0f02ed4f69b36a
Prabhat Sharma <hi.prabhat@gmail.com>
2022-07-20 15:44:03 -0700

    change segment API key

--- pkg/meta/telemetry.go
+++ pkg/meta/telemetry.go
@@ -33,5 +33,5 @@ func init() {
 		// Endpoint: "http://localhost:8080/api/v1/segment",
 	}
 
-	SEGMENT_CLIENT, _ = analytics.NewWithConfig("hQYncuWEjDJC23MnU6jHXiye5k7qP2PL", cf)
+	SEGMENT_CLIENT, _ = analytics.NewWithConfig("CGi3aENuM7L0v1jGtSC9QEhnIVmDAEFm", cf)
 }
