78f9b8540d32962667219149242092f51e8e45f8
songzhibin97 <718428482@qq.com>
2022-08-28 11:49:18 +0800

    Update config.go

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -51,7 +51,7 @@ type config struct {
 	BatchSize                 int    `env:"ZINC_BATCH_SIZE,default=1024"`
 	MaxResults                int    `env:"ZINC_MAX_RESULTS,default=10000"`
 	AggregationTermsSize      int    `env:"ZINC_AGGREGATION_TERMS_SIZE,default=1000"`
-	MaxDocumentSize           int    `env:"ZINC_MAX_DOCUMENT_SIZE_HUMAN"`             // Max size for a single document . Default = 1048576 = ( 1 MB = 1024 * 1024)
+	MaxDocumentSize           int    `env:"ZINC_MAX_DOCUMENT_SIZE"`                   // Max size for a single document . Default = 1048576 = ( 1 MB = 1024 * 1024)
 	MaxDocumentSizeHuman      string `env:"ZINC_MAX_DOCUMENT_SIZE_HUMAN,default=1mb"` // Max size for a single document . Default = 1 MB
 	WalSyncInterval           string `env:"ZINC_WAL_SYNC_INTERVAL,default=1s"`        // sync wal to disk, 1s, 10ms
 	WalRedoLogNoSync          bool   `env:"ZINC_WAL_REDOLOG_NO_SYNC,default=false"`   // control sync after every write
