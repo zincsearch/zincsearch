f282df8338627c33b47234cebd518296eefb5b99
Jonas Geiregat <jonas.grgt@gmail.com>
2022-09-01 09:15:41 +0200

    Add strings tests

--- pkg/zutils/strings_test.go
+++ pkg/zutils/strings_test.go
@@ -26,12 +26,115 @@ func TestStringToInt(t *testing.T) {
 		args args
 		want int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "should convert numeric string to int",
+			args: args{s: "1"},
+			want: 1,
+		},
+		{
+			name: "should convert numeric string containing whitespace to int",
+			args: args{s: "		1 "},
+			want: 1,
+		},
+		{
+			name: "should return 0 when given a non numeric string",
+			args: args{s: "zero"},
+			want: 0,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := StringToInt(tt.args.s); got != tt.want {
 				t.Errorf("StringToInt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIsNumeric(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "should view '123' as numeric",
+			args: args{"123"},
+			want: true,
+		},
+		{
+			name: "should view '023' as numeric",
+			args: args{"023"},
+			want: true,
+		},
+		{
+			name: "should not view '02 3' as numeric",
+			args: args{"02 3"},
+			want: false,
+		},
+		{
+			name: "should not view 'abc' as numeric",
+			args: args{"abc"},
+			want: false,
+		},
+		{
+			name: "should view unicode '２' as numeric",
+			args: args{"２"},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsNumeric(tt.args.s); got != tt.want {
+				t.Errorf("IsNumeric(%v) = %v, want %v", tt.args.s, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSliceExists(t *testing.T) {
+	type args struct {
+		set  []string
+		find string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "should not find anything in empty set []",
+			args: args{
+				set:  []string{},
+				find: "abc",
+			},
+			want: false,
+		},
+		{
+			name: "should find abc in [def,abc] set",
+			args: args{
+				set:  []string{"def", "abc"},
+				find: "abc",
+			},
+			want: true,
+		},
+		{
+			name: "should not find abc in [def,ghi] set",
+			args: args{
+				set:  []string{"def", "ghi"},
+				find: "abc",
+			},
+			want: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SliceExists(tt.args.set, tt.args.find); got != tt.want {
+				t.Errorf("SliceExists(%v, %v) = %v, want %v", tt.args.set, tt.args.find, got, tt.want)
 			}
 		})
 	}
